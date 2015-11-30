1aec49c6aee27d593d27611a70d78b32d1a47323
Huayuan Tu <tuhuayuan@gmail.com>
2015-11-30 18:14:40 +0800

    set line ending

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PASS=${MONGODB_PASS:-$(pwgen -s 12 1)}
-_word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
+_word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" ) 
 
 RET=1
 while [[ RET -ne 0 ]]; do
